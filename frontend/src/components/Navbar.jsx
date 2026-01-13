9e77b3ecbae85b821b4f8a673ec08b9140e835ee
AaRAv336 <aarav12f@gmail.com>
2026-01-13 23:37:23 +0530

    header section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,10 @@
 const Navbar = () => {
-    return <div>Navbar</div>
+    return ( 
+        <header className="bg-base-300 border-b border-base-content/10">
+<div className="mx-auto max-w-6xl p-4"> </div>
+<div className="flex items-center justify-between"></div>
+        </header>
+    
+    );
 };
 export default Navbar;
